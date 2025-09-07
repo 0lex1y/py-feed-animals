cc421101ea83fb53a0a5483006b1e0fb779b2539
0lex1y <kotov.oleksiy@icloud.com>
2025-09-07 22:08:42 +0300

    fixed appetites

--- app/main.py
+++ app/main.py
@@ -13,19 +13,19 @@ class Animal:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> int:
+        if not self.is_hungry:
+            return 0
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+
 
 
 class Cat(Animal):
     def __init__(self, name: str,
-                 appetite: int = 3,
-                 is_hungry: bool = True) -> None:
-        super().__init__(name, appetite, is_hungry)
+                 is_hungry = True) -> None:
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
@@ -33,9 +33,8 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str,
-                 appetite: int = 7,
-                 is_hungry: bool = True) -> None:
-        super().__init__(name, appetite, is_hungry)
+                 is_hungry = True) -> None:
+        super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
