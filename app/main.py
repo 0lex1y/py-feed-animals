6a86fd9f744ebda7e55c52d965ba5144519a6f88
0lex1y <kotov.oleksiy@icloud.com>
2025-09-07 22:11:57 +0300

    Fix flake8

--- app/main.py
+++ app/main.py
@@ -21,10 +21,9 @@ class Animal:
             return self.appetite
 
 
-
 class Cat(Animal):
     def __init__(self, name: str,
-                 is_hungry = True) -> None:
+                 is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
@@ -33,7 +32,7 @@ class Cat(Animal):
 
 class Dog(Animal):
     def __init__(self, name: str,
-                 is_hungry = True) -> None:
+                 is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
     def bring_slippers(self) -> None:
